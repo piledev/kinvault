7b25cdfa32487b659f4ef8a8faea08ebbb9fbf21
yokoyamada <pile.dev@gmail.com>
2024-02-22 19:00:22 +0900

    add create view.

---
+++ src/app/memo/create.tsx
@@ -0,0 +1,39 @@
+import { View, TextInput, StyleSheet, KeyboardAvoidingView } from 'react-native'
+
+import Header from '../../components/Header'
+import CircleButton from '../../components/CircleButton'
+import Icon from '../../components/Icon'
+
+const Create = (): JSX.Element => {
+  return (
+    <KeyboardAvoidingView behavior='height' style={styles.container}>
+      <Header />
+      <View style={styles.inputContainer}>
+        <TextInput multiline style={styles.input} value={''} />
+      </View>
+      <CircleButton >
+        <Icon name='check' size={40} color='#ffffff' />
+      </CircleButton>
+    </KeyboardAvoidingView>
+
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  inputContainer: {
+    flex: 1,
+    paddingVertical: 32,
+    paddingHorizontal: 27
+  },
+  input: {
+    flex: 1,
+    textAlignVertical: 'top',
+    fontSize: 16,
+    lineHeight: 24
+  }
+})
+
+export default Create
