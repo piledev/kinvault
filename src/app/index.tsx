e1da2eaf9e20db2f0004368cd7032c8cafa08884
yokoyamada <pile.dev@gmail.com>
2024-02-25 22:24:38 +0900

    implement monitoring auth state.

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,6 +1,16 @@
-import { Redirect } from 'expo-router'
+import { Redirect, router } from 'expo-router'
+import { onAuthStateChanged } from 'firebase/auth'
+import { useEffect } from 'react'
+import { auth } from '../config'
 
 const Index = (): JSX.Element => {
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user !== null) {
+        router.replace('/memo/list')
+      }
+    })
+  }, [])
   return (
     <Redirect href='auth/signup' />
   )
