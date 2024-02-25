a80de37c2849971f108ca1b231fdc4733fcbf944
yokoyamada <pile.dev@gmail.com>
2024-02-25 22:36:20 +0900

    Implement logout.

--- src/components/LogoutButton.tsx
+++ src/components/LogoutButton.tsx
@@ -1,8 +1,23 @@
-import { Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { Text, StyleSheet, TouchableOpacity, Alert } from 'react-native'
+import { signOut } from 'firebase/auth'
+import { router } from 'expo-router'
+
+import { auth } from '../config'
+
+const handlePress = (): void => {
+  signOut(auth)
+    .then(() => {
+      router.replace('/auth/login')
+    })
+    .catch((error) => {
+      const { message } = error
+      Alert.alert('Logout Error', message as string)
+    })
+}
 
 const LogoutButton = (): JSX.Element => {
   return (
-    <TouchableOpacity>
+    <TouchableOpacity onPress={handlePress}>
       <Text style={styles.text}> Logout </Text>
     </TouchableOpacity>
 
