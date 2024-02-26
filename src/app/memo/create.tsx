f36e650d2623a8182439e33bc25390a78a1c3bcc
yokoyamada <pile.dev@gmail.com>
2024-02-26 10:31:23 +0900

    Install firebase firestore.

--- src/app/memo/create.tsx
+++ src/app/memo/create.tsx
@@ -1,11 +1,19 @@
 import { View, TextInput, StyleSheet, KeyboardAvoidingView } from 'react-native'
+import { router } from 'expo-router'
+import { addDoc, collection } from 'firebase/firestore'
 
 import CircleButton from '../../components/CircleButton'
+import { db } from '../../config'
 import Icon from '../../components/Icon'
-import { router } from 'expo-router'
 
-const handleOnPress = (): void => {
-  // TODO: save
+const handlePress = (): void => {
+  addDoc(collection(db, 'memos'), { bodyText: 'test' })
+    .then((docRef) => {
+      console.log('success', docRef.id)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
   router.back()
 }
 
@@ -15,11 +23,10 @@ const Create = (): JSX.Element => {
       <View style={styles.inputContainer}>
         <TextInput multiline style={styles.input} value={'Write contents.'} />
       </View>
-      <CircleButton onPress={handleOnPress}>
+      <CircleButton onPress={handlePress}>
         <Icon name='check' size={40} color='#ffffff' />
       </CircleButton>
     </KeyboardAvoidingView>
-
   )
 }
 
